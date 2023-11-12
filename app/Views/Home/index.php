5c08fe425a2cb3a95fba1e4d42c2f54bfefcb16a
Wily96 <dlopezb24@miumg.edu.gt>
2023-11-11 23:05:22 -0600

    Add examples files VHDL

---
+++ app/Views/Home/index.php
@@ -0,0 +1,11 @@
+<?= $this->extend("layouts/default") ?>
+
+<?= $this->section("title") ?>Proyecto Algoritmos<?= $this->endSection() ?>
+
+<?= $this->section("content") ?>
+
+    <h1>Bienvenido a la pagina</h1>
+
+
+
+<?= $this->endSection() ?>
